2a17009871d63011b2a2dab661fdc5eea2f3aa06
Alan Russell <ajrussellaudio@gmail.com>
2018-11-07 21:47:07 +0000

    move example json to root

--- src/components/FilterForm/index.js
+++ src/components/FilterForm/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { addFilter } from "../../redux/actions";
+import { addFilter, removeFilter } from "../../redux/actions";
 import Checkbox from "./Checkbox";
 
 const FilterForm = props => (
   <React.Fragment>
-    <Checkbox {...props} key="Accessible24Hours" text="Has 24 hour access?" />
+    <Checkbox
+      {...props}
+      filterKey="Accessible24Hours"
+      text="Has 24 hour access?"
+    />
   </React.Fragment>
 );
 
 const mapStateToProps = state => ({ ...state });
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ addFilter }, dispatch);
+  bindActionCreators({ addFilter, removeFilter }, dispatch);
 
 export default connect(
   mapStateToProps,
